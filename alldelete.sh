338cdb11e0bf93cd9d80ffb866f045163746971b
s0wnd <aimmvp@gmail.com>
2018-07-24 08:34:13 +0900

    add delete target

--- alldelete.sh
+++ alldelete.sh
@@ -1,7 +1,9 @@
 sudo docker stop $(docker ps -aq)
 sudo docker rm $(docker ps -aq)
 sudo docker volume rm $(docker volume ls -q)
-sudo rm -rf ./provision/production/*
+sudo rm -rf ./provision
+sudo rm -rf ./generated/crypto
+sudo rm -rf ./generated/crypto-config/*
 sudo rm -rf ./generated/*.tx
 sudo rm -rf ./generated/*.block
 
